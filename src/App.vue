cabf68e8ab999718c545b9668fdb3f7915a23a13
Jonathan Hernandez <jonahernandez2001@gmail.com>
2024-09-13 19:15:27 -0600

    Initial commit to add all the files, and the contact form template

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Create your Contact List!</h1>
+
+    <section id="contact-form">
+      <h2>Add a New Contact</h2>
+      <form @submit.prevent="addContact">
+        <label for="contactName">Name:</label>
+        <input v-model="newContactName" type="text" id="contactName" required />
+
+        <label for="contactPhone">Phone Number:</label>
+        <input v-model="newContactPhone" type="tel" id="contactPhone" required />
+
+        <button type="submit">Add Contact</button>
+      </form>
+    </section>
+  </div>
+</template>
+
+
